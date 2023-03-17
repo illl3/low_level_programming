0485b41017a76aff5aa93902d2ec5fb094b8ec34
Ilyasse <superstartybo4@gmail.com>
2023-03-17 09:28:26 -0700

    Revert "j"
    This reverts commit 32c779ff8a0f9c68c05382780ade8b60a633751c.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Prints out the last part of a quote to the stderr
-*Return: (1) (success)
+*main - Prints out the last part of a quote in the stderr
+*Return: 1 .
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korper, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 58);
 	return (1);
 }
