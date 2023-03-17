fc144eac8e548288d1848f97bd70d5a825e80985
Ilyasse <superstartybo4@gmail.com>
2023-03-17 09:38:25 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Prints out the last part of a quote in the stderr
+*main - Prints out the last part of a quoteto the standard error.
 *Return: 1 .
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
