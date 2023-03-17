32c779ff8a0f9c68c05382780ade8b60a633751c
Ilyasse <superstartybo4@gmail.com>
2023-03-17 09:22:57 -0700

    j

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Prints out the last part of a quote in the stderr
-*Return: 1 .
+*main - Prints out the last part of a quote to the stderr
+*Return: (1) (success)
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 58);
+	fputs("and that piece of art is useful\" - Dora Korper, 2015-10-19\n", stdout);
 	return (1);
 }
