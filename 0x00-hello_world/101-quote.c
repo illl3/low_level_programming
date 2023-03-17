8ac5c2e2dbc4d8e2cd4ad34f7bf808698f160049
Ilyasse <superstartybo4@gmail.com>
2023-03-17 09:55:18 -0700

    you better get fixed now !

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
-*main - 101-quote.c Prints out the last part of a quote to the standard error.
-*Return: Always 0 (success) .
-*/
+ * main - 101-quote.c Prints out the last part of a quote to the standard error.
+ * Return: Always 0 (success) .
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
