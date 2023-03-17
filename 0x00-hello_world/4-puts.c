c87fb470bdf04453417a74458617cfd3a6898195
Ilyasse <superstartybo4@gmail.com>
2023-03-17 06:10:11 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-* Main - Entry point
-*
-* Return : Always 0 (Success)
-*/
+ * Main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
