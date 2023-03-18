cd53293a9cf3e2f7420172fe3dfad0a233844344
Ilyasse <superstartybo4@gmail.com>
2023-03-17 19:20:35 -0700

    j

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - main function
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int i;
+	char i;
+
+	for (i = 0 ; j < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; i <= 'f' ; i++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
