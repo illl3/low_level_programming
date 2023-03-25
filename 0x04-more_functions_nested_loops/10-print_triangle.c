0b7deaede606bc2c5db773d694f1be2033cdef80
Ilyasse <superstartybo4@gmail.com>
2023-03-24 19:38:41 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
    * print_triangle - prints a triangle, followed by a new line
     * @size: size of the triangle
@@ -23,6 +23,7 @@ void print_triangle(int size)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 			}
 		}
