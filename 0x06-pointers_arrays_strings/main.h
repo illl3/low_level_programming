aa0d1557cfb29e44850f923c949681997b29292d
Ilyasse <superstartybo4@gmail.com>
2023-03-30 13:36:35 -0700

    header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 int _puthcar(char c);
-char *_strcat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2)
 	void reverse_array(int *a, int n);
 	char *string_toupper(char *a);
