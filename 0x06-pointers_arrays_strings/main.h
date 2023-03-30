4a71466e7d24e6b3f255cad90459c8683e10dc1d
Ilyasse <superstartybo4@gmail.com>
2023-03-30 12:45:34 -0700

    header

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _puthcar(char c);
+char *_strcat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2)
+	void reverse_array(int *a, int n);
+	char *string_toupper(char *a);
+	char *cap_string(char *a);
+	char *leet(char *a);
+	char *rot13(char *a);
+	void print_number(int n);
+	char *infinit_add(char *n1, char *n2, char *r, int size_r);
+	void print_buffer(char *b, int size);
+
+#endif
