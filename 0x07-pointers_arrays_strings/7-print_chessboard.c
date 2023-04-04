a5e62479a3fd796edaaa2789f8724a912a141807
Ilyasse <superstartybo4@gmail.com>
2023-04-03 20:02:02 -0700

    str

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * print_chessboard - Entry point
-  * @a: array
-  * Return: Always 0 (Success)
-  */
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
 void print_chessboard(char (*a)[8])
 {
 	int i;
