9422dbcf8b751eeda6cf8891e4e759faa09cc5cf
Ilyasse <superstartybo4@gmail.com>
2023-04-05 11:22:14 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
